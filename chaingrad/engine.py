7f4c2781f3f4fa68862b3204239a507308e4f0f5
Bilaal <bilaalolushola@gmail.com>
2026-07-02 12:48:29 +0100

    cleaning things up

--- chaingrad/engine.py
+++ chaingrad/engine.py
@@ -74,6 +74,15 @@ class Value:
     def __sub__(self, other):
         return self + -other
     
+    def __truediv__(self, other):
+        return self * (other ** -1)
+    
+    def __rtruediv__(self, other):
+        return other * (self ** -1)
+
+    def __rsub__(self, other):
+        return -self + other
+    
     def __radd__(self, other):
         return self + other
 
