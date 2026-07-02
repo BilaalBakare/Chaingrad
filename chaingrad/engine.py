5e665cb64508bfcb7bc089cb8625c5d403ec02be
Bilaal <bilaalolushola@gmail.com>
2026-07-02 07:46:11 +0100

    fixed multiplication bug

--- chaingrad/engine.py
+++ chaingrad/engine.py
@@ -1,3 +1,5 @@
+import math
+
 class Value:
     def __init__(self, data):
         self.data = data
@@ -28,7 +30,7 @@ class Value:
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
 
-        next = self.data + other.data
+        next = self.data * other.data
         next = Value(next)
         next._op = '*'
         next._prev = {self, other}
@@ -36,6 +38,22 @@ class Value:
         def _backward_closure():
             self.grad += other.data * next.grad
             other.grad += self.data * next.grad
+        
+        next._backward = _backward_closure
+
+        return next
+    
+    def __pow__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+
+        next = self.data ** other.data
+        next = Value(next)
+        next._op = '**'
+        next._prev = {self, other}
+        
+        def _backward_closure():
+            self.grad += other.data * (self.data ** (other.data - 1)) * next.grad
+            other.grad += next.data * math.log(self.data) * next.grad
         
         next._backward = _backward_closure
 
